c366c86aa6db3d5b523f6b5fd9c1c80bd3e938a5
Burak Yalçın <burakyalcin.4510@gmail.com>
2026-04-20 02:23:25 +0300

    Skip top scroll during workflow autorun

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -2,9 +2,15 @@ import { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 
 export default function ScrollToTop() {
-  const { pathname } = useLocation()
+  const { pathname, hash, search } = useLocation()
   useEffect(() => {
+    const params = new URLSearchParams(search)
+    const shouldSkipTopScroll = hash || params.get('autorun') === '1'
+
+    if (shouldSkipTopScroll) {
+      return
+    }
     window.scrollTo(0, 0)
-  }, [pathname])
+  }, [pathname, hash, search])
   return null
 }
